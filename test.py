9a6dd2365fb61112060c6b3e09efc81549607272
Pigwiggy <s2415798@ed.ac.uk>
2026-01-25 20:08:56 +0000

    Added other files

--- test.py
+++ test.py
@@ -4,8 +4,13 @@ import pandas as pd
 import torch
 import matplotlib.pyplot as plt
 import scipy
-
+import h5py
 
 print("Hello World")
 print(sys.prefix)
 print(np.__version__)
+
+with h5py.File('data_files/new_Input_NonResonant_yy_25th_January2026.h5', 'r') as f:
+    print(list(f.keys()))
+
+
